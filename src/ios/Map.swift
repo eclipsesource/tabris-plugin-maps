8b3177d93f97a8f9576a83c01d09177ea8ceaba3
Patryk Mól <pmol@eclipsesource.com>
2015-08-05 12:21:26 +0200

    Added iOS files

---
+++ src/ios/Map.swift
@@ -0,0 +1,13 @@
+//
+//  Map.swift
+//  Tabris Maps Plugin
+//
+//  Created by Patryk Mol on 04/08/15.
+//  EclipseSource 2015
+//
+
+import Foundation
+
+@objc class Map: CDVPlugin {
+    
+}
---
+++ src/ios/MapView.swift
@@ -0,0 +1,13 @@
+//
+//  MapView.swift
+//  Tabris Maps Plugin
+//
+//  Created by Patryk Mol on 04/08/15.
+//  EclipseSource 2015
+//
+
+import Foundation
+
+class MapView: UIView {
+
+}
