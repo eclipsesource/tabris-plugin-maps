39f32df22244987bea923f22e4b8be32b6038f54
Christian Petrov <cpetrov@eclipsesource.com>
2018-01-16 20:15:52 +0100

    Support marker image property on Android
    Enable customizing the marker appearance by applying a custom image.
    
    Eiffel tower icon source [1].
    
    [1]: https://www.flaticon.com/free-icon/eiffel-tower_445624
    
    Change-Id: If29bf4ef9aed0a3143e9bc59a406a2a8d485f304

--- example/src/pages/MarkerPage.js
+++ example/src/pages/MarkerPage.js
@@ -23,7 +23,7 @@ class MarkerPage extends Page {
     }).on('ready', ({target: map}) => {
       map.region = {southWest: [48.812059, 2.2495793], northEast: [48.910537, 2.4205543]};
       this._createMarker([48.8560453, 2.3415578], 'Notre-dame');
-      this._createMarker([48.8566333, 2.2975081], 'Eifel tower');
+      this._createMarker([48.8566333, 2.2975081], 'Eifel tower', {src: 'images/eiffel-tower.png', scale: 2});
     }).appendTo(this);
 
     this._statusLabel = new TextView({
@@ -33,8 +33,8 @@ class MarkerPage extends Page {
     }).appendTo(controls);
   }
 
-  _createMarker(position, title) {
-    let marker = new esmaps.Marker({position: position})
+  _createMarker(position, title, image) {
+    let marker = new esmaps.Marker({position: position, image})
       .on('tap', () => this._statusLabel.text = 'Tapped on <b>' + title + '</b>');
     this._map.addMarker(marker);
   }
--- src/android/com/eclipsesource/tabris/maps/MarkerPropertyHandler.java
+++ src/android/com/eclipsesource/tabris/maps/MarkerPropertyHandler.java
@@ -1,11 +1,21 @@
 package com.eclipsesource.tabris.maps;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+
 import com.eclipsesource.tabris.android.Properties;
 import com.eclipsesource.tabris.android.PropertyHandler;
+import com.eclipsesource.tabris.android.TabrisContext;
+import com.eclipsesource.tabris.android.internal.toolkit.ImageProviderListenerAdapter;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 
 import java.util.List;
 
+import static com.eclipsesource.tabris.android.internal.ParamCheck.notNull;
 import static java.util.Arrays.asList;
 
 public class MarkerPropertyHandler<T extends MapMarker> implements PropertyHandler<T> {
@@ -13,11 +23,19 @@ public class MarkerPropertyHandler<T extends MapMarker> implements PropertyHandl
   private static final String PROP_POSITION = "position";
   private static final String PROP_TITLE = "title";
   private static final String PROP_SUBTITLE = "subtitle";
+  private static final String PROP_IMAGE = "image";
+
+  private final TabrisContext tabrisContext;
+
+  public MarkerPropertyHandler(TabrisContext tabrisContext) {
+    this.tabrisContext = notNull(tabrisContext, TabrisContext.class);
+  }
 
   @Override
   public void set(T marker, Properties properties) {
     setPosition(marker, properties);
     setTitle(marker, properties);
+    setImage(marker, properties);
     setSubtitle(marker, properties);
     marker.updateMarker();
   }
@@ -34,6 +52,36 @@ public class MarkerPropertyHandler<T extends MapMarker> implements PropertyHandl
     if (properties.hasProperty(PROP_TITLE)) {
       marker.getMarker().setTitle(properties.getString(PROP_TITLE));
     }
+  }
+
+  private void setImage(final T marker, Properties properties) {
+    if (properties.hasProperty(PROP_IMAGE)) {
+      List<Object> image = properties.getList(PROP_IMAGE, Object.class);
+      if (image == null) {
+        marker.setIcon(null);
+      } else {
+        this.tabrisContext.getWidgetToolkit().getImageProvider().load(image, new ImageProviderListenerAdapter() {
+            @Override
+            public void onImageLoaded(Drawable drawable) {
+              Bitmap bitmap = drawableToBitmap(drawable);
+              marker.setIcon(BitmapDescriptorFactory.fromBitmap(bitmap));
+              marker.updateMarker();
+            }
+        });
+      }
+    }
+  }
+
+  private Bitmap drawableToBitmap(Drawable drawable) {
+    if (drawable instanceof BitmapDrawable) {
+      return ((BitmapDrawable) drawable).getBitmap();
+    }
+    Canvas canvas = new Canvas();
+    Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+    canvas.setBitmap(bitmap);
+    drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+    drawable.draw(canvas);
+    return bitmap;
   }
 
   private void setSubtitle(T marker, Properties properties) {
